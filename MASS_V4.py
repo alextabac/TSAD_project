2a2db803059aab5f21d652ea771f07002b631b1c
alextabac <alextabac@gmail.com>
2023-01-20 23:39:10 +0200

    adding prints for debug

--- MASS_V4.py
+++ MASS_V4.py
@@ -27,14 +27,14 @@ class MASS_V4:
             if right >= n:
                 right = n
             dot_p = self.dct_dot_product(T[j:right], Q)
-            if sum(~np.isnan(dot_p)) > 0:
-                print(f"Found nan values from dct_dot_product: {sum(~np.isnan(dot_p))} out of {len(dot_p)}")
+            if sum(np.isnan(dot_p)) > 0:
+                print(f"Found nan values from dct_dot_product: {sum(np.isnan(dot_p))} out of {len(dot_p)}")
             sigmaT = self.movstd(T[j:right], m)
-            if sum(~np.isnan(sigmaT)) > 0:
-                print(f"Found nan values from movstd: {sum(~np.isnan(sigmaT))} out of {len(sigmaT)}")
+            if sum(np.isnan(sigmaT)) > 0:
+                print(f"Found nan values from movstd: {sum(np.isnan(sigmaT))} out of {len(sigmaT)}")
             d = np.sqrt(2.0 * (m - np.divide(dot_p, sigmaT)))  # sigmaT[m:end] in Matlab
-            if sum(~np.isnan(d)) > 0:
-                print(f"Found nan values from np.div: {sum(~np.isnan(d))} out of {len(d)}")
+            if sum(np.isnan(d)) > 0:
+                print(f"Found nan values from np.div: {sum(np.isnan(d))} out of {len(d)}")
             dist = np.concatenate((dist, d))
         return dist
 
@@ -58,26 +58,26 @@ class MASS_V4:
         n = len(x)
         m = len(y)
         x_pad, y_pad, si = self.dct_padding(x, y)
-        if sum(~np.isnan(x_pad)) > 0:
-            print(f"Found nan values in x_pad: {sum(~np.isnan(x_pad))} out of {len(x_pad)}")
-        if sum(~np.isnan(y_pad)) > 0:
-            print(f"Found nan values in y_pad: {sum(~np.isnan(y_pad))} out of {len(y_pad)}")
+        if sum(np.isnan(x_pad)) > 0:
+            print(f"Found nan values in x_pad: {sum(np.isnan(x_pad))} out of {len(x_pad)}")
+        if sum(np.isnan(y_pad)) > 0:
+            print(f"Found nan values in y_pad: {sum(np.isnan(y_pad))} out of {len(y_pad)}")
         # print(f"len(x_pad)={len(x_pad)} ; len(y_pad)={len(y_pad)} ; si={si}")
         N = len(x_pad)
         xc = dct(x_pad, type=2)
         yc = dct(y_pad, type=2)
-        if sum(~np.isnan(xc)) > 0:
-            print(f"Found nan values in xc: {sum(~np.isnan(xc))} out of {len(xc)}")
-        if sum(~np.isnan(yc)) > 0:
-            print(f"Found nan values in y_pad: {sum(~np.isnan(yc))} out of {len(yc)}")
+        if sum(np.isnan(xc)) > 0:
+            print(f"Found nan values in xc: {sum(np.isnan(xc))} out of {len(xc)}")
+        if sum(np.isnan(yc)) > 0:
+            print(f"Found nan values in y_pad: {sum(np.isnan(yc))} out of {len(yc)}")
         dct_product = np.multiply(xc, yc)
         # print(f"len(xc)={len(xc)} ; len(yc)={len(yc)} ; len(dct_product)={len(dct_product)} ; N={N}")
         dct_product.resize(N + 1)
         dct_product[N] = 0
         dct_product[0] *= sqrt(2)
         dot_p = dct(dct_product, type=1)
-        if sum(~np.isnan(dot_p)) > 0:
-            print(f"Found nan values in dot_p: {sum(~np.isnan(dot_p))} out of {len(dot_p)}")
+        if sum(np.isnan(dot_p)) > 0:
+            print(f"Found nan values in dot_p: {sum(np.isnan(dot_p))} out of {len(dot_p)}")
         # print(f"len(dot_p)={len(dot_p)}")
         dot_p[0] *= 2
         dot_p = sqrt(2 * N) * dot_p[si: si + n - m + 1]
